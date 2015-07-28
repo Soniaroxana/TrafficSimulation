27c4652fc791bc84f794773b7b96f3a78f46361c
fobofindia09 <neel.shah.528@gmail.com>
2015-07-28 18:45:02 -0500

    Multiple Intersections Test

--- src/test/LightModelTest.java
+++ src/test/LightModelTest.java
@@ -16,18 +16,6 @@ public class LightModelTest {
                 new Barrier(Arrays.asList(Direction.EAST, Direction.WEST))
         };
 
-/*
-        LightModel lightModel = new LightModel(barriers) {
-            @Override
-            public void run() {
-                while (true) {
-                    enable(barriers);
-                    disable(barriers);
-                }
-            }
-        };
-*/
-
         LightModel lightModel = new TimedLightModel(barriers, 5000L);
 
         Intersection[] intersections = new Intersection[] {
@@ -36,7 +24,6 @@ public class LightModelTest {
 
         Car[] cars = new Car[] {
                 new Car(intersections, Direction.EAST, 1000L, 0),
-                new Car(intersections, Direction.WEST, 1000L, 1)
         };
 
         Thread lightModelThread = new Thread(lightModel);
@@ -50,7 +37,7 @@ public class LightModelTest {
     }
 
     @Test
-    public void testNiceLightModel() throws InterruptedException {
+    public void testMultipleCars() throws InterruptedException {
 
         Barrier[] barriers = new Barrier[] {
                 new Barrier(Arrays.asList(Direction.NORTH, Direction.SOUTH)),
@@ -91,4 +78,72 @@ public class LightModelTest {
 
         Thread.sleep(10000L);
     }
+
+    @Test
+    public void testMultipleIntersections() throws InterruptedException {
+
+        Barrier[][] barriers = {
+                new Barrier[] {
+                        new Barrier(Arrays.asList(Direction.NORTH, Direction.SOUTH)),
+                        new Barrier(Arrays.asList(Direction.EAST, Direction.WEST))
+                },
+                new Barrier[] {
+                        new Barrier(Arrays.asList(Direction.NORTH, Direction.SOUTH)),
+                        new Barrier(Arrays.asList(Direction.EAST, Direction.WEST))
+                },
+                new Barrier[] {
+                        new Barrier(Arrays.asList(Direction.NORTH, Direction.SOUTH)),
+                        new Barrier(Arrays.asList(Direction.EAST, Direction.WEST))
+                },
+                new Barrier[] {
+                        new Barrier(Arrays.asList(Direction.NORTH, Direction.SOUTH)),
+                        new Barrier(Arrays.asList(Direction.EAST, Direction.WEST))
+                }
+        };
+
+        LightModel[] lightModels= new LightModel[] {
+                new TimedLightModel(barriers[0], 2000L),
+                new TimedLightModel(barriers[1], 3000L),
+                new TimedLightModel(barriers[2], 2000L),
+                new TimedLightModel(barriers[3], 3000L)
+        };
+
+        Intersection[] intersections = new Intersection[] {
+                new Intersection(lightModels[0], 0),
+                new Intersection(lightModels[1], 1),
+                new Intersection(lightModels[2], 2),
+                new Intersection(lightModels[3], 3)
+        };
+
+        Car[] cars = new Car[] {
+                new Car(intersections, Direction.NORTH, 1000L, 0),
+                new Car(intersections, Direction.NORTH, 2000L, 1),
+                new Car(intersections, Direction.NORTH, 2000L, 2),
+                new Car(intersections, Direction.NORTH, 2000L, 3),
+                new Car(intersections, Direction.EAST, 2000L, 4),
+                new Car(intersections, Direction.EAST, 3000L, 5),
+                new Car(intersections, Direction.EAST, 3000L, 6),
+                new Car(intersections, Direction.EAST, 3000L, 7),
+                new Car(intersections, Direction.SOUTH, 2000L, 8),
+                new Car(intersections, Direction.SOUTH, 3000L, 9),
+                new Car(intersections, Direction.SOUTH, 3000L, 10),
+                new Car(intersections, Direction.SOUTH, 3000L, 11),
+                new Car(intersections, Direction.WEST, 4000L, 12),
+                new Car(intersections, Direction.WEST, 5000L, 13),
+                new Car(intersections, Direction.WEST, 5000L, 14),
+                new Car(intersections, Direction.WEST, 5000L, 15)
+        };
+
+        for (LightModel lightModel : lightModels) {
+            new Thread(lightModel).start();
+        }
+
+        for (Car car : cars) {
+            new Thread(car).start();
+        }
+
+        Thread.sleep(15000L);
+    }
+
+
 }
